dfbe988fd3d0d889dd8a372dc98e1c95598aa999
aziz <yanoaziz@gmail.com>
2020-04-13 16:27:31 +0100

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    developement: {
+      host: "127.0.0.1",
+      port: 7545,
+      networks_id:"*"
+    }
+  }
 };
